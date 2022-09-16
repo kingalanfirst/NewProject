72c1464e81df3c28ad74dc2e28d402ada815ad4a
kingalanfirst <hyman1985@mail.ru>
2022-09-16 20:09:13 +0300

    second Name

--- NewProject/ContentView.swift
+++ NewProject/ContentView.swift
@@ -15,7 +15,9 @@ struct ContentView: View {
             .padding()
     }
 }
-
+func printSecondName() {
+    print("Artem")
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
