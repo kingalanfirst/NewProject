5abe0822e11b7488fcaae45c2ad7c04913e6ca35
kingalanfirst <hyman1985@mail.ru>
2022-09-16 19:51:33 +0300

    Initial Commit

---
+++ NewProject/NewProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewProjectApp.swift
+//  NewProject
+//
+//  Created by Alan on 16.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct NewProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
